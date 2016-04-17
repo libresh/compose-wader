d9f7ef195f17d4b7240dadbcdca73a3cebc2ff91
pierreozoux <pierre@ozoux.net>
2016-04-17 10:15:45 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# wader
+# Wader
 The migration endpoint for IndieHosters infrastructure
+
+## What
+
+This endpoint allows 2 things.
+
+1. `/download/:hash` Get an archive from your domain
+
+Imagine you are using a Software as a Service. You are worried because they might [stop their business](https://indiewebcamp.com/site-deaths).
+With IndieHosters, at what ever moment, you can get a full archive of your running web application!
+
+2. `/migrate/:hash` Unattended migration
+
+You could also decide to move from one provider to another. This is possible also, we stop properly your running web app, do the necessary dumps and send a zip.
+This will also point the current HA to your IP address (not yet implemented).
+
+## How
+
+The only thing you need is to ask your Hosting Provider the secret hash linked to your domain. This hash should be kept secret.
+(Anyone with access to it, can access your running app)
+
+## Why Wader
+
+Because there are [cute migrants birds](https://en.wikipedia.org/wiki/Wader) :) And as it is about migration!
---
+++ index.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var archiver = require('archiver');
+var config = require('./config.json');
+var fs = require('fs');
+var request = require('request');
+var p = require('path');
+
+var domain = function(res, hash) {
+  if (config[hash]) {
+    return config[hash];
+  } else {
+    res.status(404).end();
+  }
+}
+
+var archive = function(res, domain) {
+  var archive = archiver('zip');
+
+  archive.on('error', function(err) {
+    res.status(500).send({error: err.message});
+  });
+
+  //set the archive name
+  res.attachment('IndieHosted.zip');
+
+  //this is the streaming magic
+  archive.pipe(res);
+  archive.directory("/data/domains/" + domain, domain);
+  archive.finalize();
+}
+
+app.get('/:type(download|migrate)/:hash', function(req, res) {
+  if (req.params.type === 'download') {
+    archive(res, domain(res, req.params.hash));
+  } else if (req.params.type === 'migrate') {
+    request('http://172.17.0.1:8080/' + req.params.hash, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        archive(res, domain(res, req.params.hash));
+      } else {
+        res.status(500).send({ error: 'Migration failed.' });
+      }
+    })
+  }
+});
+
+app.listen(3000);
